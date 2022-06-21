6ca199ecb93bc8709668f2aaae52c3170661f41f
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-06-21 16:41:50 +0300

    Update test file

--- test/1_MultySig.js
+++ test/1_MultySig.js
@@ -1,11 +1,16 @@
-const MultiSig = artifacts.require("MultiSig")
 const { assert } = require('chai')
 const truffleAssert = require('truffle-assertions')
+const BigNumber = require("bignumber.js")
+const constants = require('@openzeppelin/test-helpers/src/constants.js')
+const Token = artifacts.require("ERC20Token")
 
-contract("Testing MultiSig Contract", accounts => {
+// potentially needs to be moved to Integrate repo
+contract("MultiSig", accounts => {
+    const initiatorAddress = accounts[1], ConfirmerAddress = accounts[2]
+    let token
 
     before(async () => {
-
+        token = await Token.new('TEST', 'TEST')
     })
 
 })
