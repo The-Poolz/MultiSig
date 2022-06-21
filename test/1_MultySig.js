30d6c8bfda6ec1142d4440284ca215a29d437d7a
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-06-21 16:12:21 +0300

    Update truffle-config

---
+++ test/1_MultySig.js
@@ -0,0 +1,11 @@
+const MultySig = artifacts.require("MultySig")
+const { assert } = require('chai')
+const truffleAssert = require('truffle-assertions')
+
+contract("Testing MultiSig Contract", accounts => {
+
+    before(async () => {
+
+    })
+
+})
