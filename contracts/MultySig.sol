16d4212dd3df09655fa9f450028de885d65b8b86
Stan Goldin <salty87@gmail.com>
2022-06-21 14:41:59 +0300

    add ConformMint

--- contracts/MultySig.sol
+++ contracts/MultySig.sol
@@ -4,23 +4,55 @@ pragma solidity ^0.8.0;
 contract MultySig {
     address InitiationAddress;
     address ConfirmerAddress;
+    uint256 Amount;
+    address TargetAddress;
+    address TokenAddress;
 
-    constructor(address Initiation, address Confirmer) {
+    constructor(address Initiation, address Confirmer, address Token) {
         InitiationAddress = Initiation;
+        ConfirmerAddress = Confirmer;
+        TokenAddress = Token;
+    }
+
+    function ChangeInitiationAddress(address Initiation) public {
+        require(Initiation != ConfirmerAddress, "can't have same address");
+        require(
+            msg.sender == InitiationAddress,
+            "only the InitiationAddress can change it"
+        );
+        InitiationAddress = Initiation;
+    }
+
+    function ChangeConfirmerAddress(address Confirmer) public {
+        require(Confirmer != InitiationAddress, "can't have same address");
+        require(
+            msg.sender == ConfirmerAddress,
+            "only the InitiationAddress can cahnbge it"
+        );
         ConfirmerAddress = Confirmer;
     }
 
-    function ChangeInitiationAddress(address Initiation) public
-    {
-        require(Initiation != ConfirmerAddress); //can't have same address
-        require(msg.sender == InitiationAddress); //only the InitiationAddress can cahnbge it
-        InitiationAddress = Initiation;
+    function InitiateMint(uint256 amount, address target) public {
+        require(
+            msg.sender == InitiationAddress,
+            "only the InitiationAddress can do this"
+        );
+        require(
+            Amount == 0 && TargetAddress == address(0),
+            "Must not be initiated"
+        );
+        Amount = amount;
+        TargetAddress = target;
     }
 
-    function ChangeConfirmerAddress(address Confirmer) public
-    {
-        require(Confirmer != InitiationAddress); //can't have same address
-        require(msg.sender == ConfirmerAddress); //only the InitiationAddress can cahnbge it
-        ConfirmerAddress = Confirmer;
+    function ConformMint(uint256 amount, address target) public {
+        require(
+            msg.sender == ConfirmerAddress,
+            "only the ConfirmerAddress can do this"
+        );
+        require(Amount == amount && TargetAddress == target,"Must use the same values from initiation");
+        ERC20(TokenAddress).Mint(target,amount);
+        Amount = 0;
+        TargetAddress = address(0);
     }
 }
