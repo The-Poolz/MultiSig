a288ffec600aed83fd32173f828ce1e3e298f3c0
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-06-21 16:39:55 +0300

    Update migration file

--- migrations/1_MultiSig.js
+++ migrations/1_MultiSig.js
@@ -1,5 +1,7 @@
-const MultiSig = artifacts.require("MultiSig.sol");
+const MultiSig = artifacts.require("MultiSig")
+const constants = require("@openzeppelin/test-helpers/src/constants")
 
 module.exports = function (deployer) {
-  deployer.deploy(MultiSig);
-};
+  deployer.deploy(MultiSig, constants.ZERO_ADDRESS, constants.ZERO_ADDRESS, constants.ZERO_ADDRESS)
+  //constructor(address Initiator, address Confirmer, address Token)
+}
