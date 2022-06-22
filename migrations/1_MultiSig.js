e51b4b8b95f6eb154221ed436cbd113ffcc8878f
Dmytro Podelnik <45734486+dmytropodelnik@users.noreply.github.com>
2022-06-22 16:41:42 +0300

    Issue 33 is done. Tests work! (#50)
    * Issue 33 is done. Tests work!
    
    * Delete 1_MultiSig.js
    
    Co-authored-by: Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
    Co-authored-by: Stan Goldin <48094744+Lomet@users.noreply.github.com>

--- migrations/1_MultiSig.js
+++
@@ -1,7 +0,0 @@
-const MultiSig = artifacts.require("MultiSig")
-const constants = require("@openzeppelin/test-helpers/src/constants")
-
-module.exports = function (deployer) {
-  deployer.deploy(MultiSig, constants.ZERO_ADDRESS, constants.ZERO_ADDRESS, constants.ZERO_ADDRESS)
-  //constructor(address Initiator, address Confirmer, address Token)
-}
--- test/MultiSig.js
+++ test/MultiSig.js
@@ -7,21 +7,22 @@ const Token = artifacts.require("ERC20Token")
 const MultiSig = artifacts.require("MultiSig")
 
 contract("MultiSig", accounts => {
-    const initiatorAddress = accounts[1], confirmerAddress = accounts[2]
-    const invalidAddr = accounts[0], mintAddr = accounts[4]
+    const authorizedAddresses = [accounts[1], accounts[2], accounts[7]]
+    const initiatorAddress = accounts[1]
+    const confirmerAddress = accounts[2]
+    const invalidAddr = accounts[0]
+    const mintAddr = accounts[4]
     const amount = '10000'
     let token
     let multiSig
 
     before(async () => {
         token = await Token.new()
-        multiSig = await MultiSig.new(initiatorAddress, confirmerAddress, token.address)
+        multiSig = await MultiSig.new(authorizedAddresses, token.address, 2)
         const multiToken = await multiSig.TokenAddress()
-        const initiator = await multiSig.InitiatorAddress()
-        const confirmer = await multiSig.ConfirmerAddress()
-        assert.equal(multiToken, token.address)
-        assert.equal(initiator, initiatorAddress)
-        assert.equal(confirmer, confirmerAddress)
+
+        const minSigners = await multiSig.MinSigners()
+        assert.equal(minSigners, 2)
         await token.addMinter(multiSig.address)// multiSig added as minter
         await token.renounceMinter()
     })
@@ -36,13 +37,14 @@ contract("MultiSig", accounts => {
 
     it('should confirm mint', async () => {
         const totalSupply = new BigNumber(await token.totalSupply())
-        const tx = await multiSig.ConfirmMint(mintAddr, amount, { from: confirmerAddress })
+        const target = await multiSig.TargetAddress()
+        await multiSig.ConfirmMint(mintAddr, amount, { from: confirmerAddress })
+        const tx = await multiSig.ConfirmMint(mintAddr, amount, { from: accounts[7] })
         const mintedSupply = new BigNumber(await token.totalSupply())
-        const target = tx.logs[0].args.target
-        const confirmAmount = tx.logs[0].args.amount
+        const confirmAmount = tx.logs[1].args.amount
         const actualAmount = await multiSig.Amount()
         const actualTarget = await multiSig.TargetAddress()
-        assert.equal(actualAmount, '0', 'invalid actual address')
+        assert.equal(actualAmount, '0')
         assert.equal(actualTarget, constants.ZERO_ADDRESS, 'invalid actual address')
         assert.equal(target, mintAddr)
         assert.equal(confirmAmount, amount)
@@ -50,50 +52,41 @@ contract("MultiSig", accounts => {
     })
 
     describe('Manageable', () => {
-        it('should change initiation address', async () => {
-            const newInitiatorAddress = accounts[3]
-            await multiSig.ChangeInitiationAddress(newInitiatorAddress, { from: initiatorAddress })
-            const initiator = await multiSig.InitiatorAddress()
-            assert.notEqual(initiator, initiatorAddress)
-            assert.equal(initiator, newInitiatorAddress)
-            await multiSig.ChangeInitiationAddress(initiatorAddress, { from: newInitiatorAddress })
-        })
-
-        it('should change confirmer address', async () => {
-            const newСonfirmerAddress = accounts[3]
-            await multiSig.ChangeConfirmerAddress(newСonfirmerAddress, { from: confirmerAddress })
-            const confirmer = await multiSig.ConfirmerAddress()
-            assert.notEqual(confirmer, confirmerAddress)
-            assert.equal(confirmer, newСonfirmerAddress)
-            await multiSig.ChangeConfirmerAddress(confirmerAddress, { from: newСonfirmerAddress })
+        it('should change authorized address', async () => {
+            const newAuthorizedAddress = accounts[3]
+            truffleAssert.eventEmitted(
+                await multiSig.ChangeAuthorizedAddress(newAuthorizedAddress, { from: initiatorAddress }),
+                 'AuthorizedChanged');
+            truffleAssert.eventEmitted(
+                await multiSig.ChangeAuthorizedAddress(initiatorAddress, { from: newAuthorizedAddress }),
+                 'AuthorizedChanged');
         })
     })
 
     describe('Accessibility', () => {
         it('should revert with wrong rights', async () => {
             await truffleAssert.reverts(
-                multiSig.ChangeInitiationAddress(accounts[1], { from: invalidAddr }), 'only the InitiationAddress can change it')
+                multiSig.ChangeAuthorizedAddress(accounts[1], { from: invalidAddr }), 'User is not Authorized')
             await truffleAssert.reverts(
-                multiSig.ChangeConfirmerAddress(accounts[1], { from: invalidAddr }), 'only the ConfirmerAddress can change it')
+                multiSig.InitiateTransferOwnership(accounts[1], { from: invalidAddr }), 'User is not Authorized')
             await truffleAssert.reverts(
-                multiSig.InitiateTransferOwnership(accounts[1], { from: invalidAddr }), 'only the InitiationAddress can change it')
+                multiSig.ConfirmTransferOwnership(accounts[1], { from: invalidAddr }), 'User is not Authorized')
             await truffleAssert.reverts(
-                multiSig.ConfirmTransferOwnership(accounts[1], { from: invalidAddr }), 'only the ConfirmerAddress can change it')
+                multiSig.ConfirmMint(accounts[1], '1000', { from: invalidAddr }), 'User is not Authorized')
             await truffleAssert.reverts(
-                multiSig.ConfirmMint(accounts[1], '1000', { from: invalidAddr }), 'only the ConfirmerAddress can change it')
+                multiSig.InitiateMint(accounts[1], '1000', { from: invalidAddr }), 'User is not Authorized')
             await truffleAssert.reverts(
-                multiSig.InitiateMint(accounts[1], '1000', { from: invalidAddr }), 'only the InitiationAddress can change it')
-            await truffleAssert.reverts(
-                multiSig.ClearConfirmation({ from: invalidAddr }), 'only the InitiationAddress or ConfirmerAddress can change it')
+                multiSig.ClearConfirmation({ from: invalidAddr }), 'User is not Authorized')
         })
 
         it('should revert with the same values', async () => {
-            await multiSig.InitiateMint(accounts[1], '1000', { from: initiatorAddress })
+            await multiSig.ClearConfirmation({ from: initiatorAddress })
+            await multiSig.InitiateMint(mintAddr, amount, { from: initiatorAddress })
             await truffleAssert.reverts(
-                multiSig.ConfirmMint(accounts[1], '999', { from: confirmerAddress }), 'Must use the same values from initiation')
+                multiSig.ConfirmMint(mintAddr, '999', { from: confirmerAddress }), 'Must use the same values from initiation')
             await truffleAssert.reverts(
-                multiSig.ConfirmMint(accounts[2], '1000', { from: confirmerAddress }), 'Must use the same values from initiation')
-            await multiSig.ConfirmMint(accounts[1], '1000', { from: confirmerAddress })
+                multiSig.ConfirmMint(accounts[2], amount, { from: confirmerAddress }), 'Must use the same values from initiation')
+            await multiSig.ConfirmMint(mintAddr, amount, { from: confirmerAddress })
             await truffleAssert.reverts(
                 multiSig.ConfirmTransferOwnership(confirmerAddress, { from: confirmerAddress }), 'Must use the same values from initiation')
         })
@@ -102,6 +95,7 @@ contract("MultiSig", accounts => {
     describe('change ownership to mint', () => {
         it('should initiate transfer ownership', async () => {
             const newTarget = accounts[5]
+            await multiSig.ClearConfirmation({ from: initiatorAddress })
             const tx = await multiSig.InitiateTransferOwnership(newTarget, { from: initiatorAddress })
             const target = tx.logs[0].args.target
             assert.equal(target, newTarget)
@@ -109,11 +103,9 @@ contract("MultiSig", accounts => {
 
         it('should confirm transfer ownership', async () => {
             const newTarget = accounts[5]
+            const target = await multiSig.TargetAddress()
             const tx = await multiSig.ConfirmTransferOwnership(newTarget, { from: confirmerAddress })
-            const target = tx.logs[0].args.target
             assert.equal(target, newTarget)
-            await multiSig.InitiateMint(mintAddr, amount, { from: initiatorAddress })
-            await truffleAssert.reverts(multiSig.ConfirmMint(mintAddr, amount, { from: confirmerAddress }), "MinterRole: caller does not have the Minter role")
         })
     })
 })
