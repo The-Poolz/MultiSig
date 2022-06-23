b4f4da895ee7b2bcb2e37828e5dc347307389cb6
Stan Goldin <salty87@gmail.com>
2022-06-23 15:25:06 +0300

    issue#73

--- contracts/MultiSigConfirmer.sol
+++ contracts/MultiSigConfirmer.sol
@@ -2,13 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "./MultiSigInitiator.sol";
-import "./TokenInterface.sol";
 
 /// @title contains confirmation requests.
 contract MultiSigConfirmer is MultiSigInitiator {
     /// @dev only authorized address can change himself
-    function ChangeAuthorizedAddress(address authorize) external OnlyAuthorized {
-        require(!AuthorizedMap[authorize], "AuthorizedMap must have unique addresses");
+    function ChangeAuthorizedAddress(address authorize)
+        external
+        OnlyAuthorized
+    {
+        require(
+            !AuthorizedMap[authorize],
+            "AuthorizedMap must have unique addresses"
+        );
         require(authorize != address(0), "Authorize address must be non-zero");
         emit AuthorizedChanged(authorize, msg.sender);
         AuthorizedMap[msg.sender] = false;
@@ -16,17 +21,13 @@ contract MultiSigConfirmer is MultiSigInitiator {
     }
 
     /// @dev collects votes to confirm mint tokens
-    /// if there are enough votes, coins will be minted  
+    /// if there are enough votes, coins will be minted
     function ConfirmMint(address target, uint256 amount)
         external
         OnlyAuthorized
         ValuesCheck(target, amount)
     {
-        _newSignature();
-        if (IsFinalSig()) {
-            _mint(target, amount);
-            ClearConfirmation();
-        }
+        _confirmMint(target, amount);
     }
 
     /// @dev transfers the right to mint tokens
@@ -35,12 +36,6 @@ contract MultiSigConfirmer is MultiSigInitiator {
         OnlyAuthorized
         ValuesCheck(target, 0)
     {
-        _newSignature();
-        if (IsFinalSig()) {
-            IERC20(TokenAddress).addMinter(target);
-            IERC20(TokenAddress).renounceMinter();
-            emit CompleteChangeOwner(target);
-            ClearConfirmation();
-        }
+        _confirmTransferOwnership(target);
     }
 }
